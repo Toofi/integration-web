4d3614875a56baed5cb76dfc3747d650cdaa46e2
Quentin <herpoel.quentin@gmail.com>
2021-06-19 11:16:52 +0200

    getproducts ok && iteration sur les prix ok

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -10,35 +10,22 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  private token: string | null = sessionStorage.getItem('trackerToken') ? sessionStorage.getItem('trackerToken') : null;
-
-  private headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${this.token}`
-  });
+  products: any;
 
   constructor(public productsService: ProductsService,
     private httpClient: HttpClient,
     private httpTracker: HttpTrackerService) { 
 
     }
-  products: Array<any> = this.productsService.getProducts();
 
   ngOnInit(): void {
-    // this.httpClient
-    //   .get<any[]>(`${this.httpTracker.getApiUrl()}/api/my-products`, { headers: this.headers, responseType: 'json', withCredentials: true })
-    //   .subscribe(element => {
-    //     this.products = element;
-    //     // for (let index = 0; index < this.products.length; index++) {
-    //     //   const prices = this.products[index].prices;
-    //     //   for (let index = 0; index < prices.length; index++) {
-    //     //     const element = prices[index].price;
-    //     //     console.log(element);
-    //       // }
-    //     // }
-    //   });
-    console.log(this.products)
+    this.productsService.getProducts().subscribe(prod => {
+      this.products = Object.values(prod);
+      console.log(this.products);
+      
+      console.log(this.products.prices);
+      
+    });
   };
 
   // this.products = this.productsService.getProducts();
