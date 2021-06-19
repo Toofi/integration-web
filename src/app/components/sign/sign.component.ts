ae169afe7c0d42b22e9aa90e91701f88de22e9fd
Quentin <herpoel.quentin@gmail.com>
2021-06-19 20:57:25 +0200

    signIn ok

--- src/app/components/sign/sign.component.ts
+++ src/app/components/sign/sign.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/interfaces/user';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-sign',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignComponent implements OnInit {
 
-  constructor() { }
+  private user: User = {
+    username: '',
+    firstName:'',
+    lastName:'',
+    emails: [],
+    password: '',
+  }
+
+  constructor(private usersService : UsersService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    this.user.username = form.value['username'];
+    this.user.firstName = form.value['firstName'];
+    this.user.lastName = form.value['lastName'];
+    this.user.emails[0] = form.value['emails'];
+    this.user.password = form.value['password'];
+    this.usersService.signIn(this.user).subscribe()
+    this.usersService.getUsers();
   }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -12,72 +12,74 @@ import { Product } from '../interfaces/product';
 })
 export class UsersService {
 
-  private token: string | null = sessionStorage.getItem('trackerToken') ? sessionStorage.getItem('trackerToken') : null;
+  // private headers = new HttpHeaders({
+  //   'Content-Type': 'application/json',
+  //   'Authorization': `Bearer ${this.token}`
+  // });
 
-  private headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${this.token}`
-  });
 
-  private signHeader = new HttpHeaders({
-    'Content-Type': 'application/x-www-form-urlencoded'
-  });
 
-  private postHeader = new HttpHeaders({
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': `Bearer ${this.token}`
-  });
+  // private postHeader = new HttpHeaders({
+  //   'Content-Type': 'application/x-www-form-urlencoded',
+  //   'Authorization': `Bearer ${this.token}`
+  // });
 
   constructor(private httpClient: HttpClient,
     private httpTracker: HttpTrackerService) { }
-//OK
+
   signIn(user: User) {
+    const signHeader = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
     const body: HttpParams = new HttpParams()
-    .set('username', user.username)
-    .set('firstName', user.firstName)
-    .set('lastName', user.lastName)
-    .set('password', user.password)
-    .set('emails[0]', user.emails[0]);
-    
+      .set('username', user.username)
+      .set('firstName', user.firstName)
+      .set('lastName', user.lastName)
+      .set('password', user.password)
+      .set('emails[0]', user.emails[0]);
+
     return this.httpClient
-      .post(`${this.httpTracker.getApiUrl()}/users`, body.toString(), { headers: this.signHeader, withCredentials: true })
-      .subscribe((value) => {
-        console.log('Logs sent' + value);
-      }, (error) => {
-        console.log(error);
-      });
+      .post(`${this.httpTracker.getApiUrl()}/users`, body.toString(), { headers: signHeader, withCredentials: true });
   };
-//OK
+
   getUsers() {
-    if (!this.token) {
-      return `Erreur, il n'y a pas de token !`;
-    }
+    const token: string | null = sessionStorage.getItem('trackerToken') ? sessionStorage.getItem('trackerToken') : null;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
     return this.httpClient
-      .get(`${this.httpTracker.getApiUrl()}/api/users`, { headers: this.headers, responseType: 'json' })
+      .get(`${this.httpTracker.getApiUrl()}/api/users`, { headers: headers, responseType: 'json' })
       .subscribe((values) => console.log(values));
   };
-//OK
+
   getUser(profileId: string | null) {
-    if (this.token === null || sessionStorage.getItem('trackerId') === null) {
-      return `Erreur, pas de token!`;
-    }
+    const token: string | null = sessionStorage.getItem('trackerToken') ? sessionStorage.getItem('trackerToken') : null;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+
     return this.httpClient
-      .get(`${this.httpTracker.getApiUrl()}/api/profile/${profileId}`, { headers: this.headers, responseType: 'json' })
+      .get(`${this.httpTracker.getApiUrl()}/api/profile/${profileId}`, { headers: headers, responseType: 'json' })
       .subscribe((values) => console.log(values));
   }
 
   putUser(user: User) {
-    console.log(user);
-    console.log(this.token + 'token');
+    const token: string | null = sessionStorage.getItem('trackerToken') ? sessionStorage.getItem('trackerToken') : null;
+    const postHeader = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `Bearer ${token}`
+    });
     const body: HttpParams = new HttpParams()
-    .set('username', user.username)
-    .set('firstName', user.firstName)
-    .set('lastName', user.lastName)
-    .set('password', user.password)
-    .set('emails[0]', user.emails[0]);
+      .set('username', user.username)
+      .set('firstName', user.firstName)
+      .set('lastName', user.lastName)
+      .set('password', user.password)
+      .set('emails[0]', user.emails[0]);
 
     return this.httpClient
-      .put<User>(`${this.httpTracker.getApiUrl()}/api/users`, body.toString(), { headers: this.postHeader, responseType: 'json', withCredentials: true })
+      .put<User>(`${this.httpTracker.getApiUrl()}/api/users`, body.toString(), { headers: postHeader, responseType: 'json', withCredentials: true })
       .subscribe((values) => {
         console.log(values)
       }, (e) => {
@@ -86,10 +88,14 @@ export class UsersService {
   }
 
   putTrackedProduct(product: Product, productId: string) {
-    console.log('TOKEN' + this.token + 'TOKEN');
+    const token: string | null = sessionStorage.getItem('trackerToken') ? sessionStorage.getItem('trackerToken') : null;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
 
     return this.httpClient
-      .put<Product>(`${this.httpTracker.getApiUrl()}/api/users/${productId}`, product, { headers: this.headers, responseType: 'json', withCredentials: true })
+      .put<Product>(`${this.httpTracker.getApiUrl()}/api/users/${productId}`, product, { headers: headers, responseType: 'json', withCredentials: true })
       .subscribe((values) => {
         console.log(values)
       }, (e) => {
